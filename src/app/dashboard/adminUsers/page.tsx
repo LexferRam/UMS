a5096c0a741250d7d5c15ad89b462581151b87e1
Lexfer Ramirez <lexferramirez@gmail.com>
2024-01-08 00:27:26 -0400

    fix errors

--- src/app/dashboard/adminUsers/page.tsx
+++ src/app/dashboard/adminUsers/page.tsx
@@ -1,17 +1,26 @@
-import { headers } from 'next/headers';
+'use client'
 import AdminUsersTable from './_components/AdminUsersTable';
+import { useQuery } from 'react-query';
+import AdmiPageSkeleton from '../adminPatients/_components/AdmiPageSkeleton';
 
-const UsersAdmin = async () => {
+const UsersAdmin = () => {
 
     const TABLE_HEAD = ["Nombre", "Correo", "Role", "Especialidad", "Estatus"];
-    const respUser = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/api/admin`, {
-        method: "GET",
-        headers: headers()
-    }
-    )
-    let users = await respUser.json()
+    // const respUser = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/api/admin`, {
+    //     method: "GET",
+    //     headers: headers()
+    // }
+    // )
+    // let users = await respUser.json()
 
-    return <AdminUsersTable headers={TABLE_HEAD} users={users} />
+    const { isLoading, error, data = [], refetch } = useQuery(['usersList'], () =>
+    fetch(`${process.env.NEXT_PUBLIC_BASE_API}/api/admin`).then(res =>
+      res.json()
+    ))
+
+    if (isLoading) return <AdmiPageSkeleton />
+
+    return <AdminUsersTable headers={TABLE_HEAD} users={data} />
 }
 
 export default UsersAdmin
