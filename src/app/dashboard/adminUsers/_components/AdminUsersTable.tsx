8ed8930dc5173fc363f8a914ff68719669b42674
Lexfer Ramirez <lexferramirez@gmail.com>
2024-01-20 10:54:57 -0400

    update

--- src/app/dashboard/adminUsers/_components/AdminUsersTable.tsx
+++ src/app/dashboard/adminUsers/_components/AdminUsersTable.tsx
@@ -27,8 +27,8 @@ const AdminUsersTable: FC<AdminUsersTableProps> = ({
     )
 
     return (
-        <div className='p-5 max-h-[700px] overflow-scroll sm:overflow-visible scrollbar-hide'>
-            <div className="h-full w-full overflow-scroll sm:overflow-visible shadow-md rounded mt-8 scrollbar-hide">
+        <div className='p-5 max-h-[700px] overflow-x-scroll sm:overflow-visible scrollbar-hide'>
+            <div className="h-full w-full overflow-x-scroll sm:overflow-visible shadow-md rounded mt-8 scrollbar-hide">
                 <table className="w-full min-w-max table-auto text-left">
                     <thead>
                         <tr>
