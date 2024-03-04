20e9d111b47efe158b8a29bb9b4b24e94b9fdf66
Lexfer <lexferramirez@gmail.com>
2024-03-03 20:43:59 -0400

    add scroll Area component to event dialog

--- src/app/dashboard/scheduler/_components/EventDetailsModal.tsx
+++ src/app/dashboard/scheduler/_components/EventDetailsModal.tsx
@@ -15,6 +15,7 @@ import EditEventModal from './EditEventModal';
 import { useUserInfo } from '@/hooks';
 import { Textarea } from '@/components/ui/textarea';
 import { useForm } from 'react-hook-form';
+import { ScrollArea } from '@/components/ui/ScrollArea';
 
 interface IEventDetailsModal {
     open: boolean
@@ -56,173 +57,176 @@ const EventDetailsModal = ({
                 setCancelEvent(false)
             }}
         >
-            <DialogContent className="sm:max-w-[425px] overflow-y-scroll scrollbar-hide">
-                {/* // ! DIALOG HEADER  */}
-                <DialogHeader className='flex flex-row gap-4 items-center justify-start'>
-                    <div>
-                        <DialogTitle>
-                            {!editEvent ? eventDetails?.title : null}
-                        </DialogTitle>
-                    </div>
+            <DialogContent className="sm:max-w-[500px]">
 
-                    {editEvent && (
-                        <div
-                            className='flex gap-2 items-center cursor-pointer'
-                            onClick={() => {
-                                setCancelEvent(false)
-                                setEditEvent(false)
-                            }} >
-                            <ArrowLeftIcon
-                                className="h-6 w-6 text-green-500"
-                            />
-                            <span
-                                className='text-sm font-semibold text-gray-600'
-                            >
-                                Volver
-                            </span>
+                <ScrollArea className='max-h-[400px] sm:max-h-[400px]'>
+                    {/* // ! DIALOG HEADER  */}
+                    <DialogHeader className='flex flex-row gap-4 items-center justify-start'>
+                        <div>
+                            <DialogTitle>
+                                {!editEvent ? eventDetails?.title : null}
+                            </DialogTitle>
                         </div>
-                    )}
-                </DialogHeader>
 
-                {/* // ! DETAILS OF THE EVENT  */}
-                {!editEvent && <>
-                    <div className='flex flex-col sm:flex-row'>
-                        <b className='mr-2'>Tipo de Evento: </b> <p>{eventDetails?.eventType}</p>
-                    </div>
+                        {editEvent && (
+                            <div
+                                className='flex gap-2 items-center cursor-pointer'
+                                onClick={() => {
+                                    setCancelEvent(false)
+                                    setEditEvent(false)
+                                }} >
+                                <ArrowLeftIcon
+                                    className="h-6 w-6 text-green-500"
+                                />
+                                <span
+                                    className='text-sm font-semibold text-gray-600'
+                                >
+                                    Volver
+                                </span>
+                            </div>
+                        )}
+                    </DialogHeader>
 
-                    <div className='flex flex-col sm:flex-row'>
-                        <b className='mr-2'>Paciente: </b> <p>{eventDetails?.patient.name + ' ' + eventDetails?.patient?.lastname}</p>
-                    </div>
+                    {/* // ! DETAILS OF THE EVENT  */}
+                    {!editEvent && <>
+                        <div className='flex flex-col sm:flex-row'>
+                            <b className='mr-2'>Tipo de Evento: </b> <p>{eventDetails?.eventType}</p>
+                        </div>
 
-                    <div className='flex flex-col sm:flex-row'>
-                        <b className='mr-2'>Fecha de Nacimiento: </b> <p>{moment(eventDetails?.patient?.dateOfBirth).format('LL')}</p>
-                    </div>
+                        <div className='flex flex-col sm:flex-row'>
+                            <b className='mr-2'>Paciente: </b> <p>{eventDetails?.patient.name + ' ' + eventDetails?.patient?.lastname}</p>
+                        </div>
 
-                    <div className='flex flex-col sm:flex-row'>
-                        <b className='mr-2'>Edad: </b> <p>{calculateAgeWithMonths(eventDetails?.patient?.dateOfBirth)?.years} años y {calculateAgeWithMonths(eventDetails?.patient?.dateOfBirth)?.months} meses</p>
-                    </div>
+                        <div className='flex flex-col sm:flex-row'>
+                            <b className='mr-2'>Fecha de Nacimiento: </b> <p>{moment(eventDetails?.patient?.dateOfBirth).format('LL')}</p>
+                        </div>
 
-                    <div className='flex flex-col sm:flex-row'>
-                        <b className='mr-2'>Diagnóstico: </b> <p>{eventDetails?.patient?.diagnosis}</p>
-                    </div>
+                        <div className='flex flex-col sm:flex-row'>
+                            <b className='mr-2'>Edad: </b> <p>{calculateAgeWithMonths(eventDetails?.patient?.dateOfBirth)?.years} años y {calculateAgeWithMonths(eventDetails?.patient?.dateOfBirth)?.months} meses</p>
+                        </div>
 
-                    <div className='flex flex-col sm:flex-row'>
-                        <b className='mr-2'>MC: </b> <p>{eventDetails?.patient?.historyDescription}</p>
-                    </div>
+                        <div className='flex flex-col sm:flex-row'>
+                            <b className='mr-2'>Diagnóstico: </b> <p>{eventDetails?.patient?.diagnosis}</p>
+                        </div>
 
-                    <div className='flex flex-col sm:flex-row'>
-                        <b className='mr-2'>Especialista asignado: </b> <p>{eventDetails?._asignTo.name}</p>
-                    </div>
+                        <div className='flex flex-col sm:flex-row'>
+                            <b className='mr-2'>MC: </b> <p>{eventDetails?.patient?.historyDescription}</p>
+                        </div>
 
-                    <div className='flex flex-col sm:flex-row'>
-                        {(userInfo[0]?.role === 'admin') ? (
-                            <div className='flex flex-col gap-2 '>
-                                <div className='flex gap-1 items-center cursor-not-allowed' onClick={() => setEditEvent(false)}>
-                                    <PencilSquareIcon
-                                        className="h-6 w-6 text-blue-500"
-                                    />
-                                    <span
-                                        className='text-sm font-semibold text-blue-600'
-                                    >
-                                        Editar el evento actual
-                                    </span>
-                                </div>
+                        <div className='flex flex-col sm:flex-row'>
+                            <b className='mr-2'>Especialista asignado: </b> <p>{eventDetails?._asignTo.name}</p>
+                        </div>
 
-                                <div className='flex gap-1 items-center cursor-pointer' onClick={() => setEditEvent(true)}>
-                                    <PencilSquareIcon
-                                        className="h-6 w-6 text-green-500"
-                                    />
-                                    <span
-                                        className='text-sm font-semibold text-green-600'
-                                    >
-                                        Editar todo el evento
-                                    </span>
-                                </div>
+                        <div className='flex flex-col sm:flex-row'>
+                            {(userInfo[0]?.role === 'admin') ? (
+                                <div className='flex flex-col gap-2 '>
+                                    <div className='flex gap-1 items-center cursor-not-allowed' onClick={() => setEditEvent(false)}>
+                                        <PencilSquareIcon
+                                            className="h-6 w-6 text-blue-500"
+                                        />
+                                        <span
+                                            className='text-sm font-semibold text-blue-600'
+                                        >
+                                            Editar el evento actual
+                                        </span>
+                                    </div>
 
-                                {
-                                    // ? si el evento no tiene reporte para la fecha seleccionada, se habilita el boton de "cancelar este evento"
-                                    eventDetails?.reports?.map((report: any) => new Date(report?.createdAt)?.toLocaleString("es-VE")?.split(',')[0])?.filter((rep: string) => rep === selectedDate?.date).length === 0 && (
+                                    <div className='flex gap-1 items-center cursor-pointer' onClick={() => setEditEvent(true)}>
+                                        <PencilSquareIcon
+                                            className="h-6 w-6 text-green-500"
+                                        />
+                                        <span
+                                            className='text-sm font-semibold text-green-600'
+                                        >
+                                            Editar todo el evento
+                                        </span>
+                                    </div>
+
+                                    {
+                                        // ? si el evento no tiene reporte para la fecha seleccionada, se habilita el boton de "cancelar este evento"
+                                        eventDetails?.reports?.map((report: any) => new Date(report?.createdAt)?.toLocaleString("es-VE")?.split(',')[0])?.filter((rep: string) => rep === selectedDate?.date).length === 0 && (
+                                            <div
+                                                className='flex gap-1 items-center cursor-pointer'
+                                                onClick={() => {
+                                                    setEditEvent(true)
+                                                    setCancelEvent(true)
+                                                }}>
+                                                <XMarkIcon
+                                                    className="h-6 w-6 text-orange-500"
+                                                />
+                                                <span
+                                                    className='text-sm font-semibold text-orange-600'
+                                                >
+                                                    Cancelar éste evento
+                                                </span>
+                                            </div>
+                                        )
+                                    }
+
+                                    {eventDetails?.reports?.map((report: any) => new Date(report?.createdAt)?.toLocaleString("es-VE")?.split(',')[0])?.filter((rep: string) => rep === selectedDate?.date).length === 0 && isRecurrentEvent && (
                                         <div
                                             className='flex gap-1 items-center cursor-pointer'
-                                            onClick={() => {
-                                                setEditEvent(true)
-                                                setCancelEvent(true)
-                                            }}>
-                                            <XMarkIcon
+                                            onClick={async () => {
+                                                try {
+
+                                                    await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/api/admin/events/${eventDetails?._id}`, {
+                                                        method: 'PUT',
+                                                        headers: {
+                                                            "Content-Type": "application/json"
+                                                        },
+                                                        body: JSON.stringify({ newEndDate })
+                                                    })
+
+                                                    setOpen(false)
+                                                    await refetchEvents()
+
+                                                } catch (error) {
+                                                    console.log(error)
+                                                }
+                                            }}
+                                        >
+                                            <TrashIcon
                                                 className="h-6 w-6 text-orange-500"
                                             />
                                             <span
                                                 className='text-sm font-semibold text-orange-600'
                                             >
-                                                Cancelar éste evento
+                                                Eliminar evento a partir de ésta cita
                                             </span>
                                         </div>
-                                    )
-                                }
+                                    )}
 
-                                {eventDetails?.reports?.map((report: any) => new Date(report?.createdAt)?.toLocaleString("es-VE")?.split(',')[0])?.filter((rep: string) => rep === selectedDate?.date).length === 0 && isRecurrentEvent && (
-                                    <div
-                                        className='flex gap-1 items-center cursor-pointer'
-                                        onClick={ async() => {
-                                            try {
-
-                                                await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/api/admin/events/${eventDetails?._id}`, {
-                                                    method: 'PUT',
-                                                    headers: {
-                                                        "Content-Type": "application/json"
-                                                    },
-                                                    body: JSON.stringify({newEndDate})
-                                                })
-
-                                                setOpen(false)
-                                                await refetchEvents()
-
-                                            } catch (error) {
-                                                console.log(error)
-                                            }
-                                        }}
-                                    >
-                                        <TrashIcon
-                                            className="h-6 w-6 text-orange-500"
-                                        />
-                                        <span
-                                            className='text-sm font-semibold text-orange-600'
-                                        >
-                                            Eliminar evento a partir de ésta cita
-                                        </span>
-                                    </div>
-                                )}
-
-                            </div>
-                        ) : null}
+                                </div>
+                            ) : null}
 
 
-                    </div>
-                </>}
+                        </div>
+                    </>}
 
-                {/* // ! EDIT THE WHOLE EVENT  */}
-                {editEvent && !cancelEvent && (
-                    <EditEventModal
-                        eventDetails={eventDetails}
-                        refetchEvents={refetchEvents}
-                        setOpen={setOpen}
-                        setEditEvent={setEditEvent}
-                        userInfo={userInfo}
-                    />
-                )}
+                    {/* // ! EDIT THE WHOLE EVENT  */}
+                    {editEvent && !cancelEvent && (
+                        <EditEventModal
+                            eventDetails={eventDetails}
+                            refetchEvents={refetchEvents}
+                            setOpen={setOpen}
+                            setEditEvent={setEditEvent}
+                            userInfo={userInfo}
+                        />
+                    )}
 
-                {/* // ! CANCEL THE EVENT  */}
-                {cancelEvent && (
-                    <CancelEventReportForm
-                        eventId={selectedDate.eventId}
-                        patient={selectedDate.patient}
-                        dateOfMissingReport={selectedDate.date}
-                        refetchEvents={refetchEvents}
-                        setOpen={setOpen}
-                        setEditEvent={setEditEvent}
-                        setCancelEvent={setCancelEvent}
-                    />
-                )}
+                    {/* // ! CANCEL THE EVENT  */}
+                    {cancelEvent && (
+                        <CancelEventReportForm
+                            eventId={selectedDate.eventId}
+                            patient={selectedDate.patient}
+                            dateOfMissingReport={selectedDate.date}
+                            refetchEvents={refetchEvents}
+                            setOpen={setOpen}
+                            setEditEvent={setEditEvent}
+                            setCancelEvent={setCancelEvent}
+                        />
+                    )}
+                </ScrollArea>
 
             </DialogContent>
 
