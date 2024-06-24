df7f6336a77ff66f122200274b610965054b8cb9
Lexfer <lexferramirez@gmail.com>
2024-06-24 18:19:09 -0400

    Refactor code to handle patients under 1 year old, improve code readability, and update dependencies

--- src/components/dashboardTabs/DashboardTabs.tsx
+++ src/components/dashboardTabs/DashboardTabs.tsx
@@ -174,29 +174,31 @@ const DashboardTabs: FC<{
                         </div>
 
                         {/* CITAS CANCELADAS CON SIN RECUPERACIONES */}
-                        {/* <div
-                            onClick={() => setSelectedCard('missingReports')}
-                            className="relative overflow-hidden p-5 bg-red-50 rounded-2xl shadow-lg hover:shadow-2xl cursor-pointer"
-                        >
-                            <div className="flex items-center space-x-2 space-y-3">
-                                <div className="flex items-center justify-center w-12 h-12 rounded-full bg-red-50 ">
-                                    <ExclamationTriangleIcon className="h-9 w-9 text-red-400" strokeWidth={2} />
+                        {userInfo[0]?.role === 'admin' && (
+                            <div
+                                onClick={() => setSelectedCard('missingReports')}
+                                className="relative overflow-hidden p-5 bg-red-50 rounded-2xl shadow-lg hover:shadow-2xl cursor-pointer"
+                            >
+                                <div className="flex items-center space-x-2 space-y-3">
+                                    <div className="flex items-center justify-center w-12 h-12 rounded-full bg-red-50 ">
+                                        <ExclamationTriangleIcon className="h-9 w-9 text-red-400" strokeWidth={2} />
+                                    </div>
+                                    <div className='flex flex-col items-center'>
+                                        <div className="text-red-800 text-center font-semibold max-w-[150px]">Citas canceladas sin recuperaciones</div>
+                                        <div className="text-2xl font-bold text-red-900">0</div>
+                                    </div>
+                                    <div>
+                                        <div className='absolute -top-1/4 -right-12 w-[100px] h-[100px] bg-red-200 rounded-full opacity-40' />
+                                        <div className='absolute -bottom-1/4 -right-16 w-[100px] h-[100px] bg-red-200 rounded-full opacity-40' />
+                                        <div className='absolute -bottom-1/4 -left-16 w-[100px] h-[100px] bg-red-200 rounded-full opacity-40' />
+                                        <div className='absolute -top-1/4 -left-16 w-[100px] h-[100px] bg-red-200 rounded-full opacity-40' />
+                                    </div>
                                 </div>
-                                <div className='flex flex-col items-center'>
-                                    <div className="text-red-800 text-center font-semibold max-w-[150px]">Citas canceladas sin recuperaciones</div>
-                                    <div className="text-2xl font-bold text-red-900">0</div>
-                                </div>
-                                <div>
-                                    <div className='absolute -top-1/4 -right-12 w-[100px] h-[100px] bg-red-200 rounded-full opacity-40' />
-                                    <div className='absolute -bottom-1/4 -right-16 w-[100px] h-[100px] bg-red-200 rounded-full opacity-40' />
-                                    <div className='absolute -bottom-1/4 -left-16 w-[100px] h-[100px] bg-red-200 rounded-full opacity-40' />
-                                    <div className='absolute -top-1/4 -left-16 w-[100px] h-[100px] bg-red-200 rounded-full opacity-40' />
-                                </div>
+
+
+
                             </div>
-
-
-
-                        </div> */}
+                        )}
 
                         {/*VER CALENDARIO */}
                         <Link
@@ -213,7 +215,7 @@ const DashboardTabs: FC<{
                                             {/* {userInfo[0]?.role === 'admin' ? 'Reportes' : 'Mis reportes'} */}
                                             Ver Calendario
                                         </div>
-                                        <div className="text-2xl font-bold text-esmerald-900">-</div>
+                                        <div className="text-2xl font-bold text-esmerald-900 h-[56px]">-</div>
                                     </div>
 
                                     <div>
